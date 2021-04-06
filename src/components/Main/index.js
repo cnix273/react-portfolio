f2f57d73bc99c5d9ba9ea9223c24ca0f5540ae57
Cameron Nix <camerondeverenix@gmail.com>
2021-04-05 23:14:10 -0700

    updated main to render About, Portfolio, and Contact components and added main styling

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,155 +1,66 @@
 import React, { Component } from "react";
-import API from "../../utils/API";
-import SearchForm from "../SearchForm";
-import SearchResults from "../SearchResults";
+
+import Portfolio from "../Portfolio";
+import Contact from "../Contact";
+import About from "../About"
+
+import "./style.css";
 
 class Search extends Component {
   state = {
-    employees: [],
-    filteredEmployees: [],
-    sorted: false
+    name: [],
+    email: [],
+    message: []
   };
-
-  // When the component mounts, get a list of all employees and update this.state.employees
-  componentDidMount() {
-    API.getEmployeeList()
-      .then(res => {
-          const employees = res.data.results;
-          employees.map(employee => {
-            employee.dob.date = employee.dob.date.split("T")[0];
-            employee.dob.date = employee.dob.date.split("-").reverse().join("-");
-            return employee.dob.date;
-          })
-          this.setState({
-            employees: employees,
-            filteredEmployees: employees
-          })
-      })
-      .catch(err => console.log(err));
-  }
   
-  // Setting state to search value
   handleInputChange = event => {
-    // Setting target value to searchParam variable
-    let searchParam = event.target.value.toLowerCase();
-    
-    const employees = this.state.employees
+    const name = event.target.name;
+    const value = event.target.value;
 
-    const filteredList = [];
-   
-    // Filter employees array in state by name based on searchParam
-    employees.filter(employee => {
-      if (employee.name.first.toLowerCase().search(searchParam) !== -1 || employee.name.last.toLowerCase().search(searchParam) !== -1) 
-      {filteredList.push(employee)}
-      return filteredList;
-    });
-   
-    // Set employees array in state equal to new filtered list of employees
     this.setState({
-      filteredEmployees: filteredList
-    });
+      [name]: [value]
+    })
+  }
 
-  };
+  // submitContact = contactInfor => {
+  //   axios.post("/apis/contact/new", {
+  //     username: userData.username,
+  //     email: userData.email,
+  //     password: userData.password
+  //   }).then(function (data) {
+  //     console.log("data stuff", data.data);
+  //     if (data.duplicateUser) {
+  //       // Replace with Modal
+  //       alert("Sorry, that username has been taken");
+  //     } else if (data.data.success) {
+  //       console.log("yay!")
+  //       this.props.authenticate();
+  //       this.setState({
+  //         redirectToReferrer: true
+  //       });
+  //     }
+  //   }.bind(this)).catch(function (err) {
+  //     console.log(err);
+  //   });
+  // }
 
-  // Sort employees array alphabetically
-  handleSort = event => {
+  // Setting state to search value
+  handleFormSubmit = event => {
     event.preventDefault();
 
-    // Get employees and sorted from state
-    const sortedEmployees = this.state.filteredEmployees
-    let sorted = this.state.sorted;
-
-    // Sorting employees alphabetically
-    if (this.state.sorted === false) {
-      sortedEmployees.sort((a,b) => {
-        let fa = a.name.first.toLowerCase();
-        let fb = b.name.first.toLowerCase();
-        let la = a.name.last.toLowerCase();
-        let lb = b.name.last.toLowerCase();
-
-        // Set sorted state to true
-        this.setState({
-          sorted: true
-        });
-        
-        // Sorting employees by first name
-        if (fa < fb) {
-          return -1;
-        }
-
-        if (fa > fb) {
-          return 1;
-        }
-
-        // If employees have the same first name, sort by last name
-        if (fa === fb) {
-          if (la < lb) {
-            return -1;
-          }
-    
-          if (la > lb) {
-            return 1;
-          }
-        }
-
-        return 0;
-
-      })
-    }
-
-    // Sorting employees reverse alphabetically
-    if (sorted === true) {
-      // Sort employees by first name in reverse alphabetical order
-      sortedEmployees.sort((a,b) => {
-        let fa = a.name.first.toLowerCase();
-        let fb = b.name.first.toLowerCase();
-        let la = a.name.last.toLowerCase();
-        let lb = b.name.last.toLowerCase();
-
-        // Set sorted state to false
-        this.setState({
-          sorted: false
-        });
-
-        // Sorting employees by first name
-        if (fa < fb) {
-          return 1;
-        }
-
-        if (fa > fb) {
-          return -1;
-        }
-        
-        // If employees have the same first name, sort by last name
-        if (fa === fb) {
-          if (la < lb) {
-            return 1;
-          }
-    
-          if (la > lb) {
-            return -1;
-          }
-        }
-
-        return 0;
-
-      })
-    }
-
-    // Set state with sorted employees
-    this.setState({filteredEmployees: sortedEmployees});
-  }
+  };
 
   render() {
     return (
       <div>
-        <SearchForm
-            handleInputChange={this.handleInputChange}
-            handleSort={this.handleSort}
-            search={this.state.search}
-        />
-        <SearchResults
-            employees={this.state.filteredEmployees}
+        <About/>
+        <Portfolio/>
+        <Contact
+          name={this.state.name}
+          email={this.state.email}
+          message={this.state.message}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
         />
       </div>
     );
