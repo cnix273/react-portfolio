3e2d614f3fe0a7756a7aa11af600a2eb23f02627
Cameron Nix <camerondeverenix@gmail.com>
2021-06-08 00:30:40 -0400

    updated about content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,10 +15,11 @@ const About = () => {
             <main className="row">
                 <div className="col-sm-12">
                     <img className="img-thumbnail" id="mainImage" src={profPic} alt="Cameron Nix headshot"/>
-                    <p>A solutions-driven software developer with a passion for creating beautiful &amp; scalable web applications built on JavaScript.</p>
+                    {/* <p>A solutions-driven software developer with a passion for creating beautiful &amp; scalable web applications built on JavaScript.</p> */}
+                    <p>An innovative &amp; solutions-driven software developer with a passion for creating beautiful, responsive &amp; scalable web applications.</p>
                     {/* <p>A life-long learner who finds joy in problem solving.</p> */}
-                    <p>An enthusiastic &amp; driven UCLA grad with a life-long affinity for solving problems. </p>
-                    <p>A natural leader and empathetic team player with strong communication skills &amp; a desire to work with and learn from others.</p>
+                    <p>An enthusiastic &amp; driven UCLA graduate with a life-long affinity for architecting thoughtful, straightfoward solutions to complex problem.</p>
+                    <p>An innate communicator, team player &amp; leader whose inherent empathy fuels a strong desire to connect with, collaborate with &amp; learn from others.</p>
                     {/* <br/>
                     <h5 className="links"><strong>Professional Links</strong></h5>
                     <hr/>
